f345da50c9efcaa654698bbb522efffd1ea8763b
Yuan xy <yuan_xin_yu@hotmail.com>
2019-11-03 21:01:17 +0800

    account show

--- app.py
+++ app.py
@@ -27,6 +27,17 @@ def get_transaction(id):
     return data
 
 
+def get_account(address):
+    appid = "00002"
+    appkey = "eyJhbGciOiJIUzUxMiJ9.eyJkYXRhIjoiZHgxeHl4MnhpIiwiaWF0IjoxNTcyMjUzNTk2LCJleHAiOjE2MDM3ODk1OTZ9.v377ejEaI0oq3KLkT0c8Z3TfF_eTe9LP41RqTcoWyU_fnw2LMhg2ykb3JgoQzJ-1P-qfzHnrgNTHn2PTOs6Bpg"
+    url = "http://localhost:8000/v1/accounts/"+address+"?appid="+appid+"&appkey="+appkey
+    r = requests.get(url)
+    if r.status_code != 200:
+        return _('Error: the service failed.')
+    data = json.loads(r.content.decode('utf-8-sig'))
+    return data
+
+
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
@@ -54,12 +65,28 @@ def transaction(id):
     raw_json = json.dumps(tx, indent=2)
     transaction_format(tx)
     tx['raw_json'] = raw_json
+    #TODO: 404
     return render_template('transaction_show.html',tx=tx)
 
 @app.route("/transactions/<int:id>.json")
 def transaction_json(id):
     tx = get_transaction(id)
     return jsonify(tx)
+
+
+@app.route("/accounts/<string:address>")
+def account(address):
+    acc = get_account(address)
+    raw_json = json.dumps(acc, indent=2)
+    acc['raw_json'] = raw_json
+    #TODO: 404
+    return render_template('account_show.html',acc=acc)
+
+
+@app.route("/accounts/<string:address>.json")
+def account_json(address):
+    acc = get_account(address)
+    return jsonify(acc)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=False)
