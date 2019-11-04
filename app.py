17b917ff174b8238f850b859e4f8d3f2bce9e78a
Yuan xy <yuan_xin_yu@hotmail.com>
2019-11-04 18:50:01 +0800

    special accounts

--- app.py
+++ app.py
@@ -100,6 +100,15 @@ def transaction_json(id):
     tx = get_transaction(id)
     return jsonify(tx)
 
+@app.route("/accounts")
+def accounts():
+    return render_template('accounts.html',
+        core_code_address='0000000000000000000000000000000000000000000000000000000000000000',
+        association_address='000000000000000000000000000000000000000000000000000000000a550c18',
+        transaction_fee_address='0000000000000000000000000000000000000000000000000000000000000fee',
+        validator_set_address='00000000000000000000000000000000000000000000000000000000000001d8'
+        )
+
 
 @app.route("/accounts/<string:address>")
 def account(address):
