753c3f5e9f0bfa6c0e05ea2df417245a41cbd613
Yuan xy <yuan_xin_yu@hotmail.com>
2019-11-10 22:27:38 +0800

    change libra_network to Libra-Network in http header

--- test/test_api.py
+++ test/test_api.py
@@ -16,7 +16,7 @@ def test_mol_api():
     assert response.status_code == 200
     assert response.headers["API-Server"] == "MoveOnLibra-API"
     assert response.headers['Access-Control-Allow-Origin'] == "*"
-    assert response.headers["libra_network"] == "testnet"
+    assert response.headers["Libra-network"] == "testnet"
     assert int(response.headers["Latest-Version"]) >= 0
     data = json.loads(response.content.decode('utf-8-sig'))
     assert data['network_name'] == "Libra TESTNET"
@@ -31,18 +31,18 @@ def test_mol_api_proxy():
     host = "http://apitest.MoveOnLibra.com"
     url = "/v1/libra/about"
     params = {}
-    headers = app.gen_api_header(False, "47.254.29.109-36765.explorer.moveonlibra.com")
-    assert headers["RealSwarm"] == "47.254.29.109-36765"
+    headers = app.gen_api_header(False, "47.254.29.109-33333.explorer.moveonlibra.com")
+    assert headers["RealSwarm"] == "47.254.29.109-33333"
     response = requests.get(host+url, params=params, headers=headers)
     assert response.status_code == 200
     assert response.headers["API-Server"] == "MoveOnLibra-API"
     assert response.headers['Access-Control-Allow-Origin'] == "*"
-    assert response.headers["libra_network"] == "47.254.29.109-36765"
+    assert response.headers["Libra-Network"] == "47.254.29.109-33333"
     assert int(response.headers["Latest-Version"]) >= 0
     data = json.loads(response.content.decode('utf-8-sig'))
     assert data['network_name'] == "Anonymous network"
     assert data["host"] == "47.254.29.109"
-    assert data["port"] == 36765
+    assert data["port"] == 33333
     assert data["core_code_address"] == "0"*64
     assert data["start_time"] <= data["latest_time"]
     assert data["total_transactions"] >= 1
